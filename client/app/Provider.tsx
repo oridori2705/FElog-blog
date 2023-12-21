39f05d101bea33a6c290c6b6d8d51bc7fd197ff3
신준혁 <oridori2705@naver.com>
2023-12-21 14:05:46 +0900

    commit: [client] 쿠키에있는 토큰을 갖고오는 기능 추가
    nookies 라이브러리 설치

--- client/app/Provider.tsx
+++ client/app/Provider.tsx
@@ -1,20 +1,43 @@
 "use client";
 
 import React, { ReactNode } from "react";
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 import { AppShell } from "@co-design/core";
 import { Header } from "@/components";
+import { setContext } from "@apollo/client/link/context";
+import nookies from "nookies";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:1337/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // const token = localStorage.getItem("token");
+  const { token } = nookies.get();
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const header = (
   <AppShell.Header height={70}>
     <Header />
   </AppShell.Header>
 );
-
-const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
-  cache: new InMemoryCache(),
-});
 
 function Provider({ children }: { children: ReactNode }) {
   return (
